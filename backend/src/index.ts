23752d3d4a76af7eb738a43cd0e8abde4131eef4
sid293 <siddheshgawade61@gmail.com>
2025-04-16 01:45:44 +0530

    cors settings

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://rolixer-rating-appfr.vercel.app/login"]
+}));
 app.use(express.json());
 
 // Routes
